4f1d73b95ea91878444d1a11f5de4daad60b1ece
aamir0143 <mdaamirreza00@gmail.com>
2022-01-26 17:01:57 +0530

    [Aamir4] Add : Added Started Game At Position 0

---
+++ SnakeAndLadderProblems/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace SnakeAndLadderProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==========Welcome To Snake And Ladder Game========");
+            SnakeAndLadder snakeAndLadder = new SnakeAndLadder();
+            snakeAndLadder.StartGame(); // Call the Method.
+            Console.ReadLine();
+        }
+    }
+}
