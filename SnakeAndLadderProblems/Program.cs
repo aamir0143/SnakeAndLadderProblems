33180b677b6e59abff7f6c8e113ad8bf3217b406
aamir0143 <mdaamirreza00@gmail.com>
2022-01-27 22:16:59 +0530

    [Aamir3] Add : Added Player Reaches the Winning Position 100

--- SnakeAndLadderProblems/Program.cs
+++ SnakeAndLadderProblems/Program.cs
@@ -8,7 +8,7 @@ namespace SnakeAndLadderProblems
         {
             Console.WriteLine("==========Welcome To Snake And Ladder Game========");
             SnakeAndLadder snakeAndLadder = new SnakeAndLadder();
-            snakeAndLadder.StartGame(); 
+            snakeAndLadder.StartGame();
             Console.ReadLine();
         }
     }
